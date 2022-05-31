af8e25c844f19037ebd6c80f741d376ec0c55e49
Adorn Choga <adornchoga@gmail.com>
2022-05-31 16:36:03 +0200

    Set up rubocop and fix linter errors

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class V1::GreetingsController < ApplicationController
   def show
-    @random_greeting = Greeting.all.sample()
-    render json:  @random_greeting
+    @random_greeting = Greeting.all.sample
+    render json: @random_greeting
   end
 end
